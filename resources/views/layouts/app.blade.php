2c2ef1f3093789f47a8eb26e1fbbf8ad7e5be100
Noptzy <nopsy.zet@gmail.com>
2024-06-13 13:08:55 +0700

    tambahan views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,3 +1,6 @@
+{{-- ini mah yang bootstreap auth bukan layouts kita --}}
+
+
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,24 @@
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Pejamas</title>
+  
+    <!-- Favicons -->
+    <link href="{{ asset ('BizLand/assets/img/favicon.png')}}" rel="icon">
+    <link href="{{ asset('BizLand/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+  
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  
+    <!-- Vendor CSS Files -->
+    <link href="{{asset('BizLand/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('BizLand/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+    <link href="{{asset('BizLand/assets/vendor/aos/aos.css')}}" rel="stylesheet">
+    <link href="{{asset('BizLand/assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+    <link href="{{asset('BizLand/assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+  
+    <!-- Main CSS File -->
+    <link href="{{asset('BizLand/assets/css/main.css')}}" rel="stylesheet">
+  </head>
