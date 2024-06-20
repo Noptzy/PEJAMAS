be4a493254f433c2882c1b3e7cd4f2e9b614d1cf
rohendiadiputra <adiputrarohendi@gmail.com>
2024-06-20 12:48:53 +0700

    ft/delete/users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -61,6 +61,9 @@ class UserController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $user = User::findOrFail($id);
+        $user->delete();
+        return redirect()->route('users.index')->with('success', 'User deleted successfully');
+
     }
 }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -16,7 +16,15 @@
                 <td>{{ $user->name }}</td>
                 <td>{{ $user->email }}</td>
                 <td>{{ $user->roles->role }}</td>
-                <td> <button type="button" class="btn btn-secondary">Edit</button> <button type="button" class="btn btn-danger">Delete</button></td>
+                <td>
+                <button type="button" class="btn btn-secondary">Edit</button>
+                    
+                    <form action="{{ route('users.destroy', $user->id) }}" method="POST" style="display:inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this user?')">Delete</button>
+                    </form>
+                </td>
             </tr>
         @endforeach
 
